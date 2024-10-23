5a1c4f5f609b21e04c19a9bf5f59c45c4a1dab59
tomalbrc <tomalbrc@me.com>
2024-10-23 22:06:30 +0200

    initial commit

---
+++ src/main/java/de/tomalbrc/heatwave/Heatwave.java
@@ -0,0 +1,11 @@
+package de.tomalbrc.heatwave;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Heatwave implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
